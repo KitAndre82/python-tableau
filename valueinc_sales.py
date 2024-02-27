a1745d7545b02b1aef620945baf1776871ac2126
KitAndre82 <101926563+KitAndre82@users.noreply.github.com>
2024-02-27 16:04:26 -0600

    Update valueinc_sales.py

--- valueinc_sales.py
+++ valueinc_sales.py
@@ -8,89 +8,60 @@ Created on Thu May 12 17:46:09 2022
 
 import pandas as pd
 
-#file_name=pd.read_csv('file.csv')
+data = pd.read_csv('transaction.csv')
 
-data=pd.read_csv('transaction.csv')
+data = pd.read_csv('transaction.csv',sep=';')
 
-data=pd.read_csv('transaction.csv',sep=';')
-
-#summary of the data
+# Summary of the data
 
 data.head()
 data.info()
-#playing around with variables
-
-var = 'Hello, world'
-
-#working with calculations
-#defining variables
-
-CostPerItem = 11.73
-SellingPricePerItem = 21.11
-NumberofItemsPurchased = 6
-
-ProfitPerItem = 21.11-11.73
-
-ProfitPerItem = SellingPricePerItem -CostPerItem
-
-ProfitPerTransaction = NumberofItemsPurchased * ProfitPerItem
-CostPerTransaction = CostPerItem * NumberofItemsPurchased
-SellingPricePerItem = SellingPricePerItem * NumberofItemsPurchased
-
-#CostPerTransaction =CostPerItem * NumberofItemsPurchased
-#variable = dataframe['column_name']
 
 CostPerItem = data['CostPerItem']
+
 NumberofItemsPurchased = data['NumberOfItemsPurchased']
+
 CostPerTransaction = CostPerItem * NumberofItemsPurchased
 
-#adding new column to datraframe
+# Adding new column to datraframe
 
 data['CostPerTransaction'] = CostPerTransaction
 
-#alternatively
+# Alternatively
+# data['CostPerTransaction'] = data['CostPerItem'] * data['NumberOfItemsPurchased']
 
-#data['CostPerTransaction'] = data['CostPerItem'] * data['NumberOfItemsPurchased']
-
-#Sales per transaction
+# Sales per transaction
 
 data['SalesPerTransaction'] = data['SellingPricePerItem'] * data['NumberOfItemsPurchased']
 
-#profit calculation = sales - cost
+# Calculating profit per transaction
 
 data['ProfitPerTransaction'] = data['SalesPerTransaction'] - data['CostPerTransaction']
 
+# Calculating markup per transaction
 #markup =(sales - cost)/cost
 
-data['Markup'] = (data['SalesPerTransaction'] - data['CostPerTransaction'])/data['CostPerTransaction']
+data['Markup'] = (data['SalesPerTransaction'] - data['CostPerTransaction']) / data['CostPerTransaction']
 
 #alternatively
 #data['Markup'] = data['ProfitPerTransaction]/data['CostPerTransaction]
 
-#rounding markup
+#rounding markup to the nearest 2 decimals
 
 data['Markup'] = round(data['Markup'],2)
 
 #combining date fields
-my_date = 'Day'+'-'+'Month'+'-'+'Year'
+my_date = 'Day' + '-' + 'Month' + '-' + 'Year'
 
-day=data['Day'].astype(str)
+day = data['Day'].astype(str)
 print(day.dtype)
-year=data['Year'].astype(str)
+
+year = data['Year'].astype(str)
 print(year.dtype)
 
-my_date= day+'-'+data['Month']+'-'+year
+my_date= day + '-' + data['Month'] + '-' + year
 
-data['Date']=my_date
-
-#using iloc to view specific colums/rows
-data.iloc[0] #views row with index=0
-data.iloc[0:3] #first three rows
-data.iloc[:5] #first five rows
-data.iloc[-5:] #last five rows
-data.head(3)
-data.iloc[:,2] #all rows but 2 columns
-data.iloc[4,2] #fourth row second column
+data['Date'] = my_date
 
 #using split to split the client keywords field
 #new_var = column.str.split('sep', expand = True)
@@ -103,12 +74,12 @@ data['ClientAge'] = split_col[0]
 data['ClientType'] = split_col[1]
 data['LengthContract'] = split_col[2]
 
-#using replace function
+#using replace function to remove square brackets
 
 data['ClientAge'] = data['ClientAge'].str.replace('[','')
 data['LengthContract'] = data['LengthContract'].str.replace(']','')
 
-#using lower function to chase case of string
+#using lower function to change case of string
 
 data['ItemDescription'] = data['ItemDescription'].str.lower()
 
@@ -121,13 +92,11 @@ seasons = pd.read_csv('value_inc_seasons.csv',sep=';')
 
 data = pd.merge(data , seasons, on = 'Month') 
 
-#dropping columns
-
-#df = df.drop['columnname' , axis = 1]
+#dropping ClientKeywords columns
 
 data = data.drop('ClientKeywords', axis = 1)
 
-#dropping multiple columns
+#dropping Year and Month columns
 
 data = data.drop(['Year', 'Month'], axis = 1)
 
