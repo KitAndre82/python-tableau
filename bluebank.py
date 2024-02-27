9af99327b2c51bf239f0a4622a580514ae09931b
KitAndre82 <101926563+KitAndre82@users.noreply.github.com>
2024-02-27 15:43:57 -0600

    Update bluebank.py

--- bluebank.py
+++ bluebank.py
@@ -41,60 +41,10 @@ loandata['dti'].describe()
 income = np.exp(loandata['log.annual.inc'])
 loandata['annualincome'] = income
 
-#working with arrays
 
-#1D array
-arr = np.array([1,2,3,4])
- 
-#0D array
-arr = np.array([4])
 
-#2D array
-arr = np.array([[1,2,3],[4,5,6]])
-
-#working with if statements
-
-def boo(a,b):
-    if b>a:
-        return True
-    return False
-    
-boo(56,43)
-
-#if statements
-fico=700
-
-if fico >= 300 and fico < 400: 
-    ficocat = 'Very Poor'
-elif fico >= 400 and fico < 600:
-    ficocat = 'Poor'
-elif fico >= 601 and fico < 660: 
-    ficocat = 'Fair' 
-elif fico >= 660 and fico < 780: 
-    ficocat = 'Good' 
-elif fico >=780: 
-    ficocat = 'Excellent'
-else:
-    ficocat ='Unknown'
-print(ficocat)
-
-#applying if statements to an entire column with the help of for loops
-
-fruits = ['apple','pear', 'banana']
-
-for item in fruits:
-    print (item)
-    
-for item in fruits:
-    print(item)
-    y = item + ' fruit'
-    print(y)
-
-for x in range(0,3):
-    y = fruits[x] + ' for sale'
-    print(y)
    
-#applying loops to the loan data
+# Categorizing FICO scores
 length=len(loandata)
 
 fico_category=[]
@@ -118,19 +68,19 @@ for x in range(0,length):
         fico_cat='Unknown'
     fico_category.append(fico_cat)
         
-#to create the row in our data  
+# To create the row in our data  
 fico_category = pd.Series(fico_category)
 loandata['fico.category'] = fico_category
 
-#conditional statements
-#df.loc[df[columname]condition, newcolumn]='value if condition is met'
+# conditional statements
+# df.loc[df[columname]condition, newcolumn]='value if condition is met'
 
 loandata.loc[loandata['int.rate']>0.12, 'int.rate.type']= 'High'
 loandata.loc[loandata['int.rate']<=0.12, 'int.rate.type']= 'Low'
 
 loandata['int.rate']
 
-#number of loans/rows by fico_category size
+# Number of loans/rows by fico_category size
 
 cat_size = loandata.groupby(loandata['fico.category']).size()
 cat_size.plot.bar(color='yellow', width=0.2)
@@ -144,7 +94,7 @@ purpose_count = loandata.groupby(loandata['purpose']).size()
 purpose_count.plot.bar()
 plt.show()
 
-#scatter plots
+# Scatter plot of annual income vs dti
 
 loandata.info()
 
